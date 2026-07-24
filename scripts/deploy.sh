c20aef0690bcb106956a7ebe56ed12ccee941634
Saqib Younas <muhammadsaqibyounas11@gmail.com>
2026-07-25 02:48:57 +0500

    Removed the Health check

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -29,33 +29,18 @@ echo "New Port    : $NEW_PORT"
 echo "Old Service : $OLD_SERVICE"
 echo "================================="
 
+
 echo "📦 Building Docker image..."
 sudo docker compose build "$TARGET_SERVICE"
+
 
 echo "▶️ Starting new container..."
 sudo docker compose up -d "$TARGET_SERVICE"
 
-echo "⏳ Waiting for application health..."
 
-HEALTHY=false
+echo "⏳ Waiting for application startup..."
+sleep 10
 
-for i in {1..24}; do
-    if curl -fs "http://127.0.0.1:${NEW_PORT}/health" >/dev/null; then
-        HEALTHY=true
-        break
-    fi
-
-    echo "Waiting... ($i/24)"
-    sleep 5
-done
-
-if [ "$HEALTHY" != "true" ]; then
-    echo "❌ Health check failed"
-    sudo docker compose logs --tail=100 "$TARGET_SERVICE"
-    exit 1
-fi
-
-echo "✅ Application is healthy"
 
 echo "🔄 Updating Nginx..."
 
@@ -63,17 +48,22 @@ sudo sed -i \
 "s/127.0.0.1:${OLD_PORT}/127.0.0.1:${NEW_PORT}/g" \
 /etc/nginx/sites-available/default
 
+
 echo "🧪 Testing Nginx..."
 sudo nginx -t
+
 
 echo "♻️ Reloading Nginx..."
 sudo systemctl reload nginx
 
+
 echo "🧹 Stopping old container..."
 sudo docker compose stop "$OLD_SERVICE"
 
+
 echo "🧹 Removing unused images..."
 sudo docker image prune -f
+
 
 echo "================================="
 echo "✅ Zero Downtime Deployment Complete"
